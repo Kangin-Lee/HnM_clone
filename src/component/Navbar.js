b1cf0ee049b496586a6861166c3cb3025a2d21f2
Kangin Lee <cjsworkddls1@naver.com>
2023-08-30 21:16:08 +0900

    login 디자인

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -3,6 +3,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUser } from "@fortawesome/free-regular-svg-icons";
 import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 import { Container } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
 
 const Navbar = () => {
   const menuList = [
@@ -15,16 +16,20 @@ const Navbar = () => {
     "Sale",
     "지속가능성",
   ];
+  const navigate = useNavigate();
+  const goToLogin = () => {
+    navigate("/login");
+  };
   return (
     <div>
       <div className="nav-top">
-        <div className="login-btn">
+        <div className="login-btn" onClick={goToLogin}>
           <FontAwesomeIcon icon={faUser} />
           <div style={{ marginLeft: "5px" }}>로그인</div>
         </div>
         <div className="search">
           <FontAwesomeIcon icon={faMagnifyingGlass} />
-          <input type="text" placeholder="상품 조회" />
+          <input type="text" placeholder="제품 검색" />
         </div>
       </div>
       <Container>
--- src/page/ProductAll.js
+++ src/page/ProductAll.js
@@ -1,11 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import ProductCard from "../component/ProductCard";
+import { Container, Row, Col } from "react-bootstrap";
 
 const ProductAll = () => {
+  const [productList, setProductList] = useState([]);
+  const getProducts = async () => {
+    let url = "http://localhost:5000/products/";
+    let reponse = await fetch(url);
+    let data = await reponse.json();
+    console.log(data);
+    setProductList(data);
+  };
+
+  useEffect(() => {
+    getProducts();
+  }, []);
   return (
     <div>
-      전체 상품 페이지
+      <Container>
+        <Row>
+          {productList.map((menu) => (
+            <Col lg={3} md={6} sm={12}>
+              <ProductCard item={menu} />
+            </Col>
+          ))}
+        </Row>
+      </Container>
     </div>
-  )
-}
+  );
+};
 
-export default ProductAll
+export default ProductAll;
